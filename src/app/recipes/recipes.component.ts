095677c0af488ecc4e686b952bbf9a804000c682
Marc Wright <marc@generalassemb.ly>
2021-04-08 17:51:15 -0500

    hard coded token is working

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -28,11 +28,36 @@ export class RecipesComponent implements OnInit {
     const user = {email: this.emailAddress, password: this.password};
     console.log(user);
     this.http
+      // BACKEND WITH CORS
       .post(`https://fathomless-falls-90499.herokuapp.com/auth/users/login`, user)
       .toPromise()
       .then(response => {
         this.token = response['jwt'];
         console.log(response, this.token);
+      });
+  }
+
+  getCategories(): void {
+    const headerDict = {
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+      'Access-Control-Allow-Headers': 'Content-Type',
+      Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJtYXJjLndyaWdodEBnbWFpbC5jb20iLCJleHAiOjE2MTc5NTUyNTEsImlhdCI6MTYxNzkxOTI1MX0.HrbJz7pcpHf_XkGObkr24baVVuV2NYinhjjlAawvlCc`
+    };
+    const requestOptions = {
+      headers: new HttpHeaders(headerDict),
+    };
+    // const headersObject = new HttpHeaders()
+    //   .set('content-type', 'application/json')
+    //   .set('Authorization', `Bearer ${this.token}`);
+    console.log(headerDict, requestOptions);
+    this.http
+      // NON CORS BACKEND
+      .get(`https://floating-beyond-06948.herokuapp.com/api/categories`, requestOptions)
+      .toPromise()
+      .then(response => {
+
+        console.log(response);
       });
   }
 
